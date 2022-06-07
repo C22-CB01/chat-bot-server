8c661a28d789f6d35b92b3f01c59b5d6d35e7146
mkadit <C2010F1030@bangkit.academy>
2022-06-08 02:03:14 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"math/rand"
+	"os"
 	"time"
 
+	firebase "firebase.google.com/go"
 	"github.com/joho/godotenv"
 	"gitlab.com/c22-cb01/chat-bot-server/internal/logger"
 	"gitlab.com/c22-cb01/chat-bot-server/pkg/api"
+	"go.uber.org/zap"
+	"google.golang.org/api/option"
 )
 
 func main() {
@@ -14,11 +19,11 @@ func main() {
 	godotenv.Load()
 	logger.SetLogger()
 
-	// ctx := context.Background()
-	// opt := option.WithCredentialsFile(os.Getenv("FIREBASE_CREDENTIAL_FILE"))
+	ctx := context.Background()
+	opt := option.WithCredentialsFile(os.Getenv("FIREBASE_CREDENTIAL_FILE"))
 
-	// firebase_app, err := firebase.NewApp(ctx, nil, opt)
-	/* if err != nil {
+	firebase_app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
 		zap.L().Fatal("error initializing app: %v\n", zap.Error(err))
 	}
 	fire_auth, err := firebase_app.Auth(ctx)
@@ -32,9 +37,8 @@ func main() {
 
 	defer func() {
 		fire_store.Close()
-	}() */
+	}()
 
-	// s := api.MakeServer(firebase_app, fire_auth, fire_store)
-	s := api.MakeServer(nil, nil, nil)
+	s := api.MakeServer(firebase_app, fire_auth, fire_store)
 	s.RunServer()
 }
